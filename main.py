6d8b4be6b418a888ea1601c788c1cf541f6de4e7
ZofiaRoszkiewicz <zofia.r1104@gmail.com>
2025-06-23 23:05:47 +0200

    Stworzenie interaktywnego systemu zarządzania siecią księgarni i ich ofertą - koniec

--- main.py
+++ main.py
@@ -315,7 +315,7 @@ def pokaz_liste_dostepnych_ksiegarni():
     Button(okno, text="Dodaj zaznaczone", command=dodaj_zaznaczone).pack(pady=10)
 
 
-
+#stworzenie ramek
 root = Tk()
 root.title("Projekt systemu do zarządzania siecią księgarni i ich ofertą")
 root.geometry("1200x750")
